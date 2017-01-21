f6e4f672d0dd17c31d3c8615e781a380c4f98fc7
Phillip Phoelich <pphoelich@gmail.com>
2017-01-21 20:41:18 +0100

    Highscores now working

---
+++ GGJ2017/Assets/Scripts/Highscore/Highscore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[Serializable]
+public class Highscore
+{
+    public string Name;
+    public int Score;
+}
---
+++ GGJ2017/Assets/Scripts/Highscore/HighscoreList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class HighscoreList : MonoBehaviour {
+
+    public static HighscoreList Instance { get; set; }
+
+    public List<Highscore> Highscores { get { return _highscores; } private set { _highscores = value; } }
+
+    [SerializeField]
+    private string _fileName = "Highscores";
+    [SerializeField]
+    private List<Highscore> _highscores;
+    private string _filePath;
+
+    public void Awake()
+    {
+        // Make sure only one highscore list exists.
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        // Get file path.
+        _filePath = Application.dataPath + "/" + _fileName + ".json";
+    }
+
+	// Use this for initialization
+	void Start () {
+	    if (File.Exists(_filePath))
+	    {
+	        Highscores = JsonUtility.FromJson<ObjectWrapper>(File.ReadAllText(_filePath)).Highscores;
+	    }
+	    else
+	    {
+	        Highscores = new List<Highscore>();
+	    }
+	}
+
+    private void OnApplicationQuit()
+    {
+        var objWrapper = new ObjectWrapper();
+        objWrapper.Highscores = Highscores;
+        File.WriteAllText(_filePath, JsonUtility.ToJson(objWrapper));
+    }
+
+    private struct ObjectWrapper
+    {
+        public List<Highscore> Highscores;
+    }
+
+}
