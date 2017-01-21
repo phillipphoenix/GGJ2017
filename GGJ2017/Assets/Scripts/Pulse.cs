4fac6bc0cf55defde03e343734a3ced59f40fc77
Oscar Losada <oscarlosu@gmail.com>
2017-01-21 16:14:03 +0100

    Shields with pulses and shooting and charging.

---
+++ GGJ2017/Assets/Scripts/Pulse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(MeshRenderer))]
+public class Pulse : MonoBehaviour {
+    public AnimationCurve scale;
+    public float lifetime = 1.0f;
+    public float alpha = 0.5f;
+    public MeshRenderer rend;
+    public Material mat;
+
+    float lived;
+
+    void Start () {
+        lived = 0;
+        transform.localScale = Vector3.one * scale.Evaluate(lived);
+        // Choose color
+        //rend.material = new Material(mat);
+        //rend.material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), alpha);
+        
+    }
+	
+	void Update () {        
+        // Grow during lifetime
+        lived += Time.deltaTime;
+        transform.localScale = Vector3.one * scale.Evaluate(lived / lifetime);
+        // Self destroy after duration
+        if(lived > lifetime) {
+            Destroy(gameObject);
+        }
+    }
+}
--- GGJ2017/Assets/Scripts/Shield.cs
+++ GGJ2017/Assets/Scripts/Shield.cs
@@ -2,82 +2,150 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
-[ExecuteInEditMode]
+[RequireComponent(typeof(Collider), typeof(MeshRenderer))]
 public class Shield : MonoBehaviour {
     public SteamVR_TrackedController controller;
-    public float duration;
-    public float cooldown;
+    
+    
     public string waveLayer = "Waves";
     [Tooltip("Duration of the haptic pulse in SECONDS.")]
     public float hapticDuration = 0.5f;
-    [Range(0.0f,1.0f)]
+    [Range(0.0f, 1.0f)]
     public float hapticStrength = 0.5f;
 
-    MeshFilter filter;
+    public Pulse pulsePrefab;
+
+    public AnimationCurve chargeCurve;
+    public float chargeDuration;
+    public Vector3 chargedScale;
+
+    public float shootDuration = 2.0f;
+    public float shootSpeed = 1.0f;
+    public Vector3 shieldOffset = new Vector3(0, 0, 5);
+
+    Collider col;
     MeshRenderer rend;
-    SteamVR_Controller.Device device;
-    Coroutine activeHapticCoroutine;
+
+    bool flying = false;
+    Transform pulse;
+
+    Coroutine activePulseCoroutine;
+    Coroutine activeChargeCoroutine;
 
     void Start() {
-        filter = GetComponent<MeshFilter>();
+        // Shield starts disabled.
+        col = GetComponent<Collider>();
         rend = GetComponent<MeshRenderer>();
-        device = SteamVR_Controller.Input((int)controller.controllerIndex);
-        //SetupForWireframeShader();
+        DisableShield();
+        
+        // Callbacks for trigger button
         if (controller) {
-            controller.TriggerClicked += EnableShield;
-            controller.TriggerUnclicked += DisableShield;
+            controller.TriggerClicked += TriggerDown;
+            controller.TriggerUnclicked += TriggerUp;
+        } else {
+            Debug.LogError(this.GetType().Name + " needs a reference to a " + typeof(SteamVR_TrackedController).Name);
         }
     }
 
-    void SetupForWireframeShader() {
-        // Set each vertex of a triangle to R G and B respectively
-        Mesh m = filter.sharedMesh;
-        m.colors = new Color[m.vertexCount];
-        for (int i = 0; i < m.triangles.Length; ++i) {
-            int tri = m.triangles[i];
-            if (i % 3 == 0) {
-                m.colors[tri] = Color.red;
-            } else if (i % 3 == 1) {
-                m.colors[tri] = Color.green;
-            } else {
-                m.colors[tri] = Color.blue;
-            }
+    void Update() {
+        // Follow controller
+        if(controller != null && !flying) {
+            transform.position = controller.transform.position + controller.transform.TransformDirection(shieldOffset);
+            transform.rotation = controller.transform.rotation;
         }
-        rend.material = new Material(Shader.Find("Custom/Wireframe"));
     }
 
-    void EnableShield(object sender, ClickedEventArgs e) {
-
-    }
-
-    void DisableShield(object sender, ClickedEventArgs e) {
-
-    }
-
-    private void OnDisable() {
+    void OnDisable() {
         if (controller) {
-            controller.TriggerClicked -= EnableShield;
-            controller.TriggerUnclicked -= DisableShield;
+            controller.TriggerClicked -= TriggerDown;
+            controller.TriggerUnclicked -= TriggerUp;
         }
     }
 
-    private void OnTriggerEnter(Collider other) {
-        if(other.gameObject.layer == LayerMask.NameToLayer(waveLayer)) {
-            if(activeHapticCoroutine == null) {
-                activeHapticCoroutine = StartCoroutine(StartHapticVibrationCoroutine(hapticDuration, hapticStrength));
-            }            
-            Destroy(other.gameObject);
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer(waveLayer)) {
+            HandleWaveCollision(col.ClosestPointOnBounds(other.transform.position), other.gameObject);
         }
     }
 
-    IEnumerator StartHapticVibrationCoroutine(float duration, float strength) {
+    void TriggerDown(object sender, ClickedEventArgs e) {
+        ChargeShield();
+    }
 
-        for (float i = 0; i < duration; i += Time.deltaTime) {
-            device.TriggerHapticPulse((ushort)Mathf.Lerp(0, 3999, strength));
+    void TriggerUp(object sender, ClickedEventArgs e) {        
+        ShootShield();
+    }    
+
+    void HandleWaveCollision(Vector3 collisionCenter, GameObject wave) {        
+        Destroy(wave);
+        // Haptic feedback
+        // TODO
+        // Visual feedback
+        Pulse(collisionCenter);
+    }
+
+    [ContextMenu("Test Charge Shield")]
+    void ChargeShield() {
+        if (activeChargeCoroutine == null && !flying) {
+            activeChargeCoroutine = StartCoroutine(ChargeShieldCo());
+        }        
+    }
+
+    IEnumerator ChargeShieldCo() {
+        // Enable
+        EnableShield();
+        // Scale
+        for (float t = 0; t < chargeDuration; t += Time.deltaTime) {
+            transform.localScale = chargedScale * chargeCurve.Evaluate(t / chargeDuration);
             yield return null;
         }
+        activeChargeCoroutine = null;
+    }
 
-        activeHapticCoroutine = null;
+    [ContextMenu("Test Shoot Shield")]
+    void ShootShield() {
+        if (!flying) {
+            // Stop charging and shoot shield
+            if(activeChargeCoroutine != null) {
+                StopCoroutine(activeChargeCoroutine);
+            }            
+            activeChargeCoroutine = null;
+            StartCoroutine(ShootShieldCo());
+        }
+    }
+
+    IEnumerator ShootShieldCo() {
+        // Scale
+        flying = true;
+        for (float t = 0; t < shootDuration; t += Time.deltaTime) {
+            transform.position += shootSpeed * Time.deltaTime * transform.forward;
+            yield return null;
+        }
+        flying = false;
+        DisableShield();
+    }
+
+    void Pulse(Vector3 center) {
+        // Instantiate pulse as child
+        if(pulsePrefab != null) {
+            Pulse p = Instantiate(pulsePrefab);
+            p.transform.parent = transform;
+            p.transform.position = center;
+
+
+        }        
+    }
+
+    
+
+    void DisableShield() {
+        rend.enabled = false;
+        col.enabled = false;
+        transform.localScale = Vector3.one * chargeCurve.Evaluate(0);
+    }
+
+    void EnableShield() {
+        rend.enabled = true;
+        col.enabled = true;
     }
 }
