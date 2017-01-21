a0f2a2ecd62575cb7b0e6b6887f6ccd80a3328c9
Andreas Lagerstedt <AndreasLagerstedt@Andreass-MacBook-Pro-2.local>
2017-01-21 12:37:22 +0100

    Ocean and Planet

---
+++ GGJ2017/Assets/Scripts/Rotator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	Transform t;
+	public float speed;
+	float delay;
+	// Use this for initialization
+	void Start () {
+		t = transform;
+		delay = Random.Range (0f, 5f);
+		speed += Random.Range (-10f, 10f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		delay -= 0.1f;
+
+		if(delay < 0)
+		t.Rotate (Vector3.down * Time.deltaTime * speed);
+	}
+}
