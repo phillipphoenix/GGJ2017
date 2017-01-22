5f73b9852685c9d77822122cedda58cfc0f90d96
Phillip Phoelich <pphoelich@gmail.com>
2017-01-22 12:11:38 +0100

    Fixed wave instantiator.

--- GGJ2017/Assets/Scripts/WaveboxInstantiator.cs
+++ GGJ2017/Assets/Scripts/WaveboxInstantiator.cs
@@ -2,34 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WaveboxInstantiator : MonoBehaviour {
+public class WaveboxInstantiator : MonoBehaviour
+{
 
     [SerializeField]
-	private GameObject _prefab;
+    private GameObject _prefab;
     [SerializeField]
-	private int _waveLength, _waveDepth, _segments;
-	// Use this for initialization
-	void Start () {
-		Wave ();
-	}
+    private int _waveLength, _waveDepth;
+    // Use this for initialization
+    void Start()
+    {
+        Wave();
+    }
 
-	void Wave (){
+    void Wave()
+    {
         var wave = new GameObject("Wave");
-	    wave.transform.position = transform.position;
-       // wave.transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, -45-180, 0));
+        wave.transform.position = transform.position;
+        wave.transform.rotation = Quaternion.Euler(transform.eulerAngles + new Vector3(0, 90, 0));
         wave.transform.parent = transform;
 
-
-		for(int s = 1; s < _segments + 1; s++){
-			for (int i = 0; i <_waveLength; i++){
-				for(int k = 0; k<_waveDepth; k++){
-					var box = Instantiate (_prefab);
-        	       	box.transform.parent = wave.transform;
-					box.transform.localPosition = new Vector3(i, transform.position.y, k + (s * _waveDepth));
-					box.GetComponent<Wavebox> ().delay = (i * 0.5f) + (s*2); // + (k * 0.5f);
-					}
-				}
-		}
-	}
+        int halfLength = _waveLength / 2;
+        int halfDepth = _waveDepth / 2;
+        for (int i = -halfLength; i < halfLength; i++)
+        {
+            for (int k = -halfDepth; k < halfDepth; k++)
+            {
+                var box = Instantiate(_prefab);
+                box.transform.parent = wave.transform;
+                box.transform.localPosition = new Vector3(i, transform.position.y, k);
+                box.GetComponent<Wavebox>().delay = (i * 0.5f); // + (k * 0.5f);
+            }
+        }
+    }
 }
 
